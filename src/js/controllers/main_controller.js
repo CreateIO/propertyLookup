6818adffa08a54d9365b72c39832f10fde73e75f
Laura Ferguson <allenderl@acm.org>
2015-02-27 14:39:00 -0500

    add some fields

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,5 +1,7 @@
 angular.module('PropLookupRE.controllers.Main', [])
 
 .controller('MainController', function($scope){
+        
+        $scope.address = "315 S. Garfield St, Arlington, VA 22204"
   
 });
