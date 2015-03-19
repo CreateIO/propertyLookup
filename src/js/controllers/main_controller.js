78a184f012eeae5dad2749591d1829625a4db84e
justboris <boriskan@gmail.com>
2015-03-19 17:31:29 -0400

    test

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -2,6 +2,10 @@ angular.module('PropLookupRE.controllers.Main', [])
 
 .controller('MainController', function($scope){        
 	//$scope.currentAddress = "1111 19th St NW, Washington, DC 20036";
-	$scope.address = "315 S. Garfield St, Arlington, VA 22204"
-  
+	$scope.address2 = "315 S. Garfield St, Arlington, VA 22204";
+  	 $scope.issue = function(){
+  		var textElement = document.getElementById("householdIncome");
+  		textElement.innerHTML = 'no more issue!';  
+  		console.log(textElement.innerHTML);		
+  	};
 });
