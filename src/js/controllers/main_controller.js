51eeca87737e3ff926181707428685d66147204c
justboris <boriskan@gmail.com>
2015-03-20 16:09:26 -0400

    add capitalize filter to fix error

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -8,4 +8,55 @@ angular.module('PropLookupRE.controllers.Main', [])
   		textElement.innerHTML = 'no more issue!';  
   		console.log(textElement.innerHTML);		
   	};
+
+//    TweetMax.to('.navbar-brand-center', 2, {left: 500});
+
+})
+  .filter('capitalize', function () {
+    return function (input, format) {
+      if (!input) {
+        return input;
+      }
+      format = format || 'all';
+      if (format === 'first') {
+        // Capitalize the first letter of a sentence
+        return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+      } else {
+        var words = input.split(' ');
+        var result = [];
+        words.forEach(function(word) {
+          if (word.length === 2 && format === 'nottwo' && word.toLowerCase() !== 'of' && word.toLowerCase() !== 'st') {
+            // Uppercase team abbreviations like FC, CD, SD except of and st
+            result.push(word.toUpperCase());
+          } else {
+            result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+          }
+        });
+        return result.join(' ');
+      }
+    };
+  })
+
+.directive('smoothButton', function(){
+    var linker = function (scope, element, attrs) {
+        var tl = new TimelineLite();
+        tl.add(TweenLite.to(element.find('.red'), 0.4, {scaleX:1.8, scaleY:1.8, ease: Power2.easeOut}));
+        tl.add(TweenLite.to(element.find('.orange'), 0.4, {scaleX:1.6, scaleY:1.6, ease: Power2.easeOut}), '-=0.2');
+        tl.add(TweenLite.to(element.find('.yellow'), 0.4, {scaleX:1.4, scaleY:1.4, ease: Power2.easeOut}), '-=0.2');
+        tl.stop();
+
+        scope.play = function() {
+            tl.play();
+        };
+
+        scope.reverse = function() {
+            tl.reverse();
+        };
+    };
+
+    return {
+        scope: true,
+        link: linker,
+//        templateUrl: 'smooth-button.tmpl.html'
+    }
 });
