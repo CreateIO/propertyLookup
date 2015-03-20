fbb05fbfca0b4b70187baf578e5a97d79b5e3285
Laura Ferguson <allenderl@acm.org>
2015-03-20 16:17:25 -0400

    changes by boris?

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -4,4 +4,15 @@ angular.module('PropLookupRE.controllers.Main', [])
 		$scope.currentAddress = "1111 19th St NW, Washington, DC 20036";
         $scope.address = "315 S. Garfield St, Arlington, VA 22204"
   
-});
+}) //https://github.com/winkerVSbecks/locator/blob/master/package.json
+    .controller('MainCtrl', ['$scope', 'location', function ($scope, location) {
+        location.get(angular.noop, angular.noop);
+        $scope.isModalVisible = false;
+
+        $scope.toggleModal = function() {
+            $scope.isModalVisible = !$scope.isModalVisible;
+        };
+
+        $scope.$watch('pickedLocation', $scope.toggleModal);
+        $scope.$watch('lookedUpLocation', $scope.toggleModal);
+    }]);
