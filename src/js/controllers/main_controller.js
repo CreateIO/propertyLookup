ae6c1d94649d3ab32aa4ac4ada1d4c0989904997
justboris <boriskan@gmail.com>
2015-03-20 16:33:08 -0400

    clean up

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,7 +1,7 @@
 angular.module('PropLookupRE.controllers.Main', [])
 
 .controller('MainController', function($scope){        
-	//$scope.currentAddress = "1111 19th St NW, Washington, DC 20036";
+	$scope.currentAddress = "1111 19th St NW, Washington, DC 20036";
 	$scope.address2 = "315 S. Garfield St, Arlington, VA 22204";
   	 $scope.issue = function(){
   		var textElement = document.getElementById("householdIncome");
