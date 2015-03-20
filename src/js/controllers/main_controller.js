696d7315c08b287ddd5faf91d689e2f6d8f84e35
Laura Ferguson <allenderl@acm.org>
2015-03-20 16:34:58 -0400

    add locator

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -2,13 +2,13 @@ angular.module('PropLookupRE.controllers.Main', [])
 
 .controller('MainController', function($scope){        
 	//$scope.currentAddress = "1111 19th St NW, Washington, DC 20036";
-	$scope.address2 = "315 S. Garfield St, Arlington, VA 22204";
+	$scope.address3 = "315 S. Garfield St, Arlington, VA 22204";
   	 $scope.issue = function(){
   		var textElement = document.getElementById("householdIncome");
   		textElement.innerHTML = 'no more issue!';  
   		console.log(textElement.innerHTML);		
   	};
-}); //https://github.com/winkerVSbecks/locator/blob/master/package.json
+}) //https://github.com/winkerVSbecks/locator/blob/master/package.json
     .controller('MainCtrl', ['$scope', 'location', function ($scope, location) {
         location.get(angular.noop, angular.noop);
         $scope.isModalVisible = false;
